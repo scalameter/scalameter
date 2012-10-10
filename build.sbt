98644456663d9cb20e48613fe23ed91834e82ab6
Aleksandar Prokopec <axel22@gmail.com>
2012-10-10 11:13:51 +0200

    Add the statistics stubs.

--- build.sbt
+++ build.sbt
@@ -10,3 +10,4 @@ libraryDependencies += "org.scalatest" % "scalatest_2.10.0-M6" % "1.9-2.10.0-M6-
 
 libraryDependencies += "jfree" % "jfreechart" % "1.0.12"
 
+libraryDependencies += "org.apache.commons" % "commons-math" % "2.0"
---
+++ src/main/scala/org/collperf/statistics.scala
@@ -0,0 +1,24 @@
+package org.collperf
+
+
+
+import collection._
+
+
+
+/* statistics */
+object statistics {
+
+  def ANOVA(history: Seq[Seq[Long]], newest: Seq[Long]): Boolean = {
+    false
+  }
+
+  def CI(alt1: Seq[Long], alt2: Seq[Long]): Boolean = {
+    false
+  }
+
+  def CoV(measurements: Seq[Long]): Boolean = {
+    false
+  }
+
+}
