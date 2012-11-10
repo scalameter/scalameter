1148affa0934fc520dcf4e617885d09fe6c1bc0d
Aleksandar Prokopec <brijest@phoenix.(none)>
2012-11-10 21:15:53 +0100

    Change Parameters.toString.

--- src/main/scala/org/scalameter/package.scala
+++ src/main/scala/org/scalameter/package.scala
@@ -204,6 +204,8 @@ package scalameter {
   case class Parameters(axisData: immutable.ListMap[String, Any]) {
     def ++(that: Parameters) = Parameters(this.axisData ++ that.axisData)
     def apply[T](key: String) = axisData.apply(key).asInstanceOf[T]
+
+    override def toString = s"Parameters(${axisData.map(t => t._1 + " -> " + t._2).mkString(, )})"
   }
 
   object Parameters {
