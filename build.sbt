ee882576f0a5db53dbe6ab223fd27093bc6f75f2
Aleksandar Prokopec <axel22@gmail.com>
2014-02-18 17:57:25 +0100

    Fixing the publishing procedure.

--- build.sbt
+++ build.sbt
@@ -1,59 +1,6 @@
+
 name := "scalameter"
 
 organization := "com.github.axel22"
 
 version := "0.5-SNAPSHOT"
-
-scalaVersion := "2.10.2"
-
-scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature")
-
-resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-
-libraryDependencies ++= List(
-  "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
-  "jfree" % "jfreechart" % "1.0.12",
-  "org.apache.commons" % "commons-math3" % "3.0",
-  "org.scala-tools.testing" % "test-interface" % "0.5"
-)
-
-
-
-
-
-publishMavenStyle := true
-
-publishTo <<= version { (v: String) =>
-  val nexus = "https://oss.sonatype.org/"
-  if (v.trim.endsWith("SNAPSHOT"))
-    Some("snapshots" at nexus + "content/repositories/snapshots")
-  else
-    Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-}
-
-publishArtifact in Test := false
-
-pomIncludeRepository := { _ => false }
-
-pomExtra := (
-  <url>http://axel22.github.com/scalameter/</url>
-  <licenses>
-    <license>
-      <name>BSD-style</name>
-      <url>http://opensource.org/licenses/BSD-3-Clause</url>
-      <distribution>repo</distribution>
-    </license>
-  </licenses>
-  <scm>
-    <url>git@github.com:axel22/scalameter.git</url>
-    <connection>scm:git:git@github.com:axel22/scalameter.git</connection>
-  </scm>
-  <developers>
-    <developer>
-      <id>axel22</id>
-      <name>Aleksandar Prokopec</name>
-      <url>http://axel22.github.com/</url>
-    </developer>
-  </developers>)
-
-
--- project/Build.scala
+++ project/Build.scala
@@ -6,43 +6,6 @@ import java.io.File
 
 
 object ScalaMeterBuild extends Build {
-  
-  /* tasks and settings */
-  
-  val javaCommand = TaskKey[String](
-    "java-command",
-    "Creates a java vm command for launching a process."
-  )
-  val javaCommandSetting = javaCommand <<= (
-    dependencyClasspath in Compile,
-    artifactPath in (Compile, packageBin),
-    artifactPath in (Test, packageBin),
-    packageBin in Compile,
-    packageBin in Test
-  ) map {
-    (dp, jar, testjar, pbc, pbt) => // -XX:+UseConcMarkSweepGC  -XX:-DoEscapeAnalysis -XX:MaxTenuringThreshold=12 -XX:+PrintGCDetails 
-    //val cp = dp.map("\"" + _.data + "\"") :+ ("\"" + jar +"\"") :+ ("\"" + testjar + "\"")
-    val cp = dp.map(_.data) :+ jar :+ testjar
-    val javacommand = "java -Xmx2048m -Xms2048m -XX:+UseCondCardMark -verbose:gc -server -cp %s".format(
-      cp.mkString(File.pathSeparator)
-    )
-    javacommand
-  }
-  
-  val runsuiteTask = InputKey[Unit](
-    "runsuite",
-    "Runs the benchmarking suite."
-  ) <<= inputTask {
-    (argTask: TaskKey[Seq[String]]) =>
-    (argTask, javaCommand) map {
-      (args, jc) =>
-      val javacommand = jc
-      val comm = javacommand + " " + "org.scalameter.Main" + " " + args.mkString(" ")
-      println("Executing: " + comm)
-      val exitcode = comm!;
-      if (exitcode != 0) error("Tests not passing.")
-    }
-  }
 
   val publishUser = "SONATYPE_USER"
   
@@ -55,18 +18,63 @@ object ScalaMeterBuild extends Build {
 
   val publishCreds: Seq[Setting[_]] = Seq(userPass match {
     case Some((user, pass)) =>
-      credentials += Credentials("Sonatype Nexus Repository Manager", "nexus.scala-tools.org", user, pass)
+      credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", user, pass)
     case None =>
       // prevent publishing
       publish <<= streams.map(_.log.info("Publishing to Sonatype is disabled since the \"" + publishUser + "\" and/or \"" + publishPass + "\" environment variables are not set."))
   })
 
+  val scalaMeterSettings = Defaults.defaultSettings ++ publishCreds ++ Seq(
+    scalaVersion := "2.10.2",
+    scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
+    resolvers ++= Seq(
+      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases"
+    ),
+    libraryDependencies ++= List(
+      "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
+      "jfree" % "jfreechart" % "1.0.12",
+      "org.apache.commons" % "commons-math3" % "3.0",
+      "org.scala-tools.testing" % "test-interface" % "0.5"
+    ),
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra :=
+      <url>http://scalameter.github.io/</url>
+      <licenses>
+        <license>
+          <name>BSD-style</name>
+          <url>http://opensource.org/licenses/BSD-3-Clause</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:axel22/scalameter.git</url>
+        <connection>scm:git:git@github.com:axel22/scalameter.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>axel22</id>
+          <name>Aleksandar Prokopec</name>
+          <url>http://axel22.github.com/</url>
+        </developer>
+      </developers>
+  )
+  
   /* projects */
   
   lazy val scalameter = Project(
     "scalameter",
     file("."),
-    settings = Defaults.defaultSettings ++ Seq(runsuiteTask, javaCommandSetting) ++ publishCreds
+    settings = scalaMeterSettings
   ) dependsOn (
   )
   
