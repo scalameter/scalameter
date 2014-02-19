70290c8a8b0639bf4b8e0078211d9188a2df3c94
Aleksandar Prokopec <axel22@gmail.com>
2014-02-19 10:40:28 +0100

    Refined the performance tests.

--- src/main/scala/org/scalameter/PerformanceTest.scala
+++ src/main/scala/org/scalameter/PerformanceTest.scala
@@ -67,6 +67,9 @@ object PerformanceTest {
     regex.findFirstIn(fullname) != None
   }
 
+  /** Quick benchmark run in the same JVM.
+   *  Reports result into the console.
+   */
   trait Quickbenchmark extends PerformanceTest {
     def executor = new execution.LocalExecutor(
       Executor.Warmer.Default(),
@@ -78,6 +81,9 @@ object PerformanceTest {
     def persistor = Persistor.None
   }
 
+  /** A more reliable benchmark run in a separate JVM.
+   *  Reports results into the console.
+   */
   trait Microbenchmark extends PerformanceTest {
     import Executor.Measurer
     def warmer = Executor.Warmer.Default()
@@ -91,9 +97,58 @@ object PerformanceTest {
     def persistor = Persistor.None
   }
 
-  trait Regression extends PerformanceTest {
+  /** A base for benchmarks generating a more detailed (regression) report, potentially online.
+   */
+  trait HTMLReport extends PerformanceTest {
     import Executor.Measurer
     import reporting._
+    def persistor = new persistence.SerializationPersistor
+    def warmer = Executor.Warmer.Default()
+    def aggregator = Aggregator.average
+    def measurer: Measurer = new Measurer.IgnoringGC with Measurer.PeriodicReinstantiation with Measurer.OutlierElimination with Measurer.RelativeNoise
+    def executor: Executor = new execution.SeparateJvmsExecutor(warmer, aggregator, measurer)
+    def tester: RegressionReporter.Tester
+    def historian: RegressionReporter.Historian
+    def online: Boolean
+    def reporter: Reporter = Reporter.Composite(
+      new RegressionReporter(tester, historian),
+      HtmlReporter(online)
+    )
+  }
+
+
+  /** Runs in separate JVM, performs regression tests and prepares HTML document for online hosting.
+   */
+  trait OnlineRegressionReport extends HTMLReport {
+    import reporting._
+    def tester = RegressionReporter.Tester.OverlapIntervals()
+    def historian = RegressionReporter.Historian.ExponentialBackoff()
+    def online = true
+  }
+
+  /** Runs in separate JVM, performs regression tests and prepares an offline HTML document.
+   */
+  trait OfflineRegressionReport extends HTMLReport {
+    import reporting._
+    def tester = RegressionReporter.Tester.OverlapIntervals()
+    def historian = RegressionReporter.Historian.ExponentialBackoff()
+    def online = false
+  }
+
+  /** Runs in separate JVM and prepares an offline HTML document.
+   *  Does not regression testing.
+   */
+  trait OfflineReport extends HTMLReport {
+    import reporting._
+    def tester = RegressionReporter.Tester.Accepter()
+    def historian = RegressionReporter.Historian.ExponentialBackoff()
+    def online = false
+  }
+
+  @deprecated("This performance test is now deprecated, please use `OnlineRegressionReport` instead.", "0.5")
+  trait Regression {
+    import reporting._
+    import Executor.Measurer
     def warmer = Executor.Warmer.Default()
     def aggregator = Aggregator.average
     def measurer: Measurer = new Measurer.IgnoringGC with Measurer.PeriodicReinstantiation with Measurer.OutlierElimination with Measurer.RelativeNoise
@@ -105,8 +160,6 @@ object PerformanceTest {
   }
 
 }
-
-
 
 
 
