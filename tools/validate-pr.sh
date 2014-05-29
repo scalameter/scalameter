d6d14b39a9861380534b6eb71d89fcb5d9ea9785
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2014-05-29 14:19:18 +0200

    Some logging.

--- tools/validate-pr.sh
+++ tools/validate-pr.sh
@@ -6,12 +6,15 @@ TRAVIS_SCALA_VERSION_ARG=$1
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]];
 then
+	echo "Testing before publishing the snapshot"
 	sbt test
 	if [ $? -eq 0 ]; then
+		echo "Publishing snapshot..."
 		sbt scalameter-core/publish publish
 	else
-		echo "Tests failed!"
+		echo "Tests failed -- no snapshot will be published!"
 	fi
 else
+	echo "Pull request -- testing without publishing the snapshot."
 	sbt $TRAVIS_SCALA_VERSION_ARG test
 fi
