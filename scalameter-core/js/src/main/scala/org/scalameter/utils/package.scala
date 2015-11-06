6124a04e41317f3734e3933c545b0c4f6f2617c4
Damien Engels <engelsdamien@gmail.com>
2015-11-06 19:44:15 +0100

    Remove use of unknown class to scalajs runtime

--- scalameter-core/js/src/main/scala/org/scalameter/utils/package.scala
+++ scalameter-core/js/src/main/scala/org/scalameter/utils/package.scala
@@ -1,15 +1,13 @@
 package org.scalameter
 
 
-
-import javax.management._
-
 /** TODO : Decide wheather it would make sense 
  *  (is possible) to have GC notifications on a
  *  js virtual machine
  */
 
 package object utils {
+  type Notification = Any
 
   def withGCNotification[T](eventhandler: Notification => Any)(body: => T) = {
     body
