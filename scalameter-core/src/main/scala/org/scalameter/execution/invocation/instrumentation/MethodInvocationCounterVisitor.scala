592e95670f31365357209d0ef29048a66cd571cd
axel22 <aleksandar.prokopec@gmail.com>
2016-04-04 23:26:14 +0200

    Wrap lines to 88.

--- scalameter-core/src/main/scala/org/scalameter/execution/invocation/instrumentation/MethodInvocationCounterVisitor.scala
+++ scalameter-core/src/main/scala/org/scalameter/execution/invocation/instrumentation/MethodInvocationCounterVisitor.scala
@@ -1,39 +1,60 @@
 package org.scalameter.execution.invocation.instrumentation
 
+
+
 import org.objectweb.asm.{Type, ClassVisitor, MethodVisitor, Opcodes}
 import org.scalameter.execution.invocation.InvocationCountMatcher
 import scala.collection.mutable
 
 
-/** Walks over methods in a class and instruments ones that are matched by [[org.scalameter.execution.invocation.InvocationCountMatcher]].
+
+/** Walks over methods in a class and instruments ones that are matched by
+ *  [[org.scalameter.execution.invocation.InvocationCountMatcher]].
  *
- * @param cv [[org.objectweb.asm.ClassVisitor]] that actually visits classes and methods
- * @param pattern selects methods that needs to be instrumented
- * @param counterClass class of counting class
- * @param counterMethod method name of counting class
- * @param initialIndex counting table start index
+ *  @param cv             [[org.objectweb.asm.ClassVisitor]] that visits classes and
+ *                        methods
+ *  @param pattern        selects methods that needs to be instrumented
+ *  @param counterClass   class of counting class
+ *  @param counterMethod  method name of counting class
+ *  @param initialIndex   counting table start index
  */
-private[instrumentation] class MethodInvocationCounterVisitor(cv: ClassVisitor, pattern: InvocationCountMatcher,
-                                                              counterClass: String, counterMethod: String,
-                                                              initialIndex: Int)
-  extends ClassVisitor(Opcodes.ASM5, cv) {
-  private val rawMethods: mutable.ArrayBuffer[MethodSignature] = mutable.ArrayBuffer.empty[MethodSignature]
+private[instrumentation] class MethodInvocationCounterVisitor(
+  cv: ClassVisitor, pattern: InvocationCountMatcher,
+  counterClass: String, counterMethod: String,
+  initialIndex: Int
+) extends ClassVisitor(Opcodes.ASM5, cv) {
+  private val rawMethods: mutable.ArrayBuffer[MethodSignature] =
+    mutable.ArrayBuffer.empty[MethodSignature]
   private var className: String = _
 
-  override def visit(version: Int, access: Int, name: String, signature: String, superName: String, interfaces: Array[String]): Unit = {
+  override def visit(
+    version: Int, access: Int, name: String, signature: String, superName: String,
+    interfaces: Array[String]
+  ): Unit = {
     className = name.replace('/', '.')
     super.visit(version, access, name, signature, superName, interfaces)
   }
 
-  /** Inserts [[counterClass]].[[counterMethod]]([[initialIndex]]) call at the beginning of a method that matches a [[pattern]]. */
-  override def visitMethod(access: Int, name: String, desc: String, signature: String, exceptions: Array[String]): MethodVisitor = {
+  /** Inserts [[counterClass]].[[counterMethod]]([[initialIndex]]) call at the beginning
+   *  of a method that matches a [[pattern]].
+   */
+  override def visitMethod(access: Int, name: String, desc: String, signature: String,
+    exceptions: Array[String]): MethodVisitor = {
     require(className != null)
 
     val mv = cv.visitMethod(access, name, desc, signature, exceptions)
-    if (className != counterClass && pattern.methodMatches(name, desc) && (access & Opcodes.ACC_ABSTRACT) == 0) {
+    if (
+      className != counterClass &&
+      pattern.methodMatches(name, desc) &&
+      (access & Opcodes.ACC_ABSTRACT) == 0
+    ) {
+      println("Instrumenting: " + className + ", method: " + name + ", desc" + desc)
       mv.visitLdcInsn(initialIndex + rawMethods.length)
-      mv.visitMethodInsn(Opcodes.INVOKESTATIC, counterClass, counterMethod, "(I)V", false)
-      val method = MethodSignature(className = className, methodName = name, Type.getArgumentTypes(desc).map(_.getClassName): _*)
+      mv.visitMethodInsn(
+        Opcodes.INVOKESTATIC, counterClass, counterMethod, "(I)V", false)
+      val method = MethodSignature(
+        className = className, methodName = name,
+        Type.getArgumentTypes(desc).map(_.getClassName): _*)
       rawMethods += method
     }
 
