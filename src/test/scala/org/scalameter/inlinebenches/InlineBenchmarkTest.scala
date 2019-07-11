fa76af09aa9cabd6d94cbf779090014f4accae68
Marcin Wisnicki <mwisnicki@gmail.com>
2019-07-10 22:10:09 -0400

    Add inline benchmarking methods that return result value to resolve #214

--- src/test/scala/org/scalameter/inlinebenches/InlineBenchmarkTest.scala
+++ src/test/scala/org/scalameter/inlinebenches/InlineBenchmarkTest.scala
@@ -16,6 +16,14 @@ class InlineBenchmarkTest extends FunSuite {
     println(s"Total time: $time")
   }
 
+  test("Should return result of measured function") {
+    val (result, time) = measured {
+      for (i <- 0 until 100000) yield i
+    }
+    println(s"Total time: $time")
+    assert(result.length == 100000)
+  }
+
   test("Should correctly execute an inline benchmark with warming") {
     val time = config(
       Key.exec.benchRuns -> 20,
